a62b68b567a9fe3ea81939935d8519d23e50b079
Jameson Rader <jameson@cueaudio.com>
2019-06-20 12:03:18 -0700

    Improved Demo applications on iOS and Android

---
+++ Android/consumer/README.md
@@ -0,0 +1,7 @@
+# Android Engine Consumer
+
+You can build project from CLI using `./gradlew build` or open Android Studio if you need that.
+
+In Android Studio if you try to run and see edit configuration dialog then
+go to `File -> Project Structure -> App` choose already choosen `Build Tools Version` 
+(Yes that's strange), tap ok and then try to run again.
---
+++ iOS/consumer/consumer/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  objC_consumer
+//
+//  Created by CUEAudio on 01/03/2018.
+//  © CUEAudio, 2018 to the last syllable of recorded time, all rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define API_KEY @"H7v7NMMNh6im735w331iLHtqnduxGCTL"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
